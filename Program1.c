1519b4fee39492125d361166e536a10cd6e991fd
jsingh2412 <60803017+jsingh2412@users.noreply.github.com>
2023-02-11 22:00:40 -0600

    finish Program1 (maybe)
    added remaining functions

--- Program1.c
+++ Program1.c
@@ -12,7 +12,7 @@ int main()
 {
 	FILE* ptr;
 	int numOfInputs = 12;
-	char* month[] = { "January",
+	char* months[] = { "January",
 					"February",
 					"March",
 					"April",
@@ -31,16 +31,72 @@ int main()
 	//	printf("File can't be opened \n");
 	//	return 0;
 	//}
-	for(int i; i<numOfInputs; i++)
+	for(int i=0; i<numOfInputs; i++)
 	{
 		fscanf(ptr,"%lf",&costsPerMonths[i]);
 	}
 	fclose(ptr);
 	printf("Month       Sales\n");
-	for(int i; i<numOfInputs; i++)
+	for(int i=0; i<numOfInputs; i++)
 	{
-		printf("%s       %lf\n",month[i], costsPerMonths[i]);
+		printf("%-12s%.2lf\n",months[i], costsPerMonths[i]);
+	}
+	double min,max,minIndex,maxIndex;
+	double avg = 0;
+	min=costsPerMonths[0];
+	max=costsPerMonths[0];
+	minIndex = 0;
+	maxIndex = 0;
+	for(int i=0; i<numOfInputs; i++)
+	{
+		avg+=costsPerMonths[i];
+		if(min>costsPerMonths[i])
+		{
+			min = costsPerMonths[i];
+			minIndex=i;
+		}
+		if(max<costsPerMonths[i])
+		{
+			max = costsPerMonths[i];
+			maxIndex=i;
+		}
+	}
+	printf("Sales summary:\n\n");
+	printf("Minimum sales:     $%.2lf  (%s)\n",min,months[minIndex]);
+	printf("Maximum sales:     $%.2lf  (%s)\n",max,months[maxIndex]);
+	printf("Minimum sales:     $%.2lf\n",avg/12);
+
+	printf("\n\nSix-Month Moving Average Report:\n\n");
+	for(int i=0; i<7; i++)
+	{
+	    avg=0;
+		for(int j=i; j<i+6; j++)
+    	{
+    		avg+=costsPerMonths[j];
+    	}
+    	printf("%-10s-  %-10s $%.2lf\n", months[i], months[i+5], avg/6);
 	}
 
+	printf("\n\nSales Report (Highest to Lowest):\n");
+	printf("  Month      Sales\n");
+	int newmax;
+	printf(" %-10s$%.2lf\n", months[maxIndex], costsPerMonths[maxIndex]);
+	int monthsRemaining=11;
+	while(monthsRemaining>0)
+	{
+		newmax=costsPerMonths[0];
+		for(int j=0; j<12; j++)
+		{
+		    if(costsPerMonths[j]<max && costsPerMonths[j]>newmax)
+		    {
+		        newmax = costsPerMonths[j];
+		        maxIndex = j;
+		    }
+		}
+		max = newmax;
+		monthsRemaining--;
+		printf(" %-10s$%.2lf\n", months[maxIndex], costsPerMonths[maxIndex]);
+	}
+	
 	return 0;
 }
